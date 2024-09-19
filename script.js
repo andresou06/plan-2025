1706ac5167218268d39bc9d1a3d6bae8523c4f22
andre_linux <andre_linux@Andreas.>
2024-09-19 10:36:25 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// const faqItems = document.querySelectorAll('.main');
+
+// faqItems.forEach(item => {
+//   const question = item.querySelector('.toggle');
+//   const answers = item.querySelector('.content');
+
+//   question.addEventListener('click', () => {
+//     // Toggle the display property to show or hide the answer
+//     faqItems.forEach(otherItem => {
+//       const otherAnswer = otherItem.querySelector('.content');
+//       if (otherAnswer !== answers) {
+//         otherAnswer.style.display = 'none'; // Close other answers
+//       }
+//     });
+
+//     if (answers.style.display === 'block') {
+//       answers.style.display = 'none';
+//     } else {
+//       answers.style.display = 'block' ;
+//     }
+//   });
+// });
